debbf82ec36f58612fb999135f8e3dfbae2e2ee4
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-08-13 12:56:46 +0300

    /

--- What's that Whistle?/ViewController.swift
+++ What's that Whistle?/ViewController.swift
@@ -11,7 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "What's that Whistle?"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWhistle))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+        
+    }
+    
+    
+    @objc func addWhistle(){
+        let vc = RecordWhistleViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 
